807b92762135b90c3366a42557d0b77394e1bdf5
Talha Mohsin <talhamohsin82@gmail.com>
2025-11-22 22:24:10 +0500

    add more

--- signup.js
+++ signup.js
@@ -4,9 +4,14 @@ function signup() {
       let email = document.getElementById("email").value;
       let password = document.getElementById("password").value;
 
+      if(password.length < 8){
+        alert("Password should be atleast 8 digits long!");
+      }
+      
       if (!name || !email || !password) {
         alert("Please fill all the fields!");
       } else {
-        alert("Signup successful (UI only)");
+        alert("Signup successful!");
+        window.location = "login.html";
       }
     }
