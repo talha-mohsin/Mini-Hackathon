26235847a78203885c04dd84bb420700f8cd56b0
Talha Mohsin <talhamohsin82@gmail.com>
2025-11-23 17:27:18 +0500

    add more work

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-function profile() {
-  let userDetails = JSON.parse(localStorage.getItem("signupUsers")) || [];
-  let loggedinUser = JSON.parse(localStorage.getItem("loggedinUser")) || [];
+// function profile() {
+//   let userDetails = JSON.parse(localStorage.getItem("signupUsers")) || [];
+//   let loggedinUser = JSON.parse(localStorage.getItem("loggedinUser")) || [];
 
-  if (!loggedinUser) {
-    return (window.location = "login.html");
-  }
-}
-profile();
+//   if (!loggedinUser) {
+//     return (window.location = "login.html");
+//   }
+// }
+// profile();
+
+let postDescription = document.querySelector('#postDescription');
+let postUrl = document.querySelector('#postUrl');
+
+document.querySelector('.post').addEventListener('click', function () {
+  // postDescription.value 
+  // postUrl.value 
+})
+let posts = document.querySelector('.posts');
